ecb4e955a8945ea3f663b3dbefcbcd21a1311d26
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-28 08:08:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,3 +10,20 @@ export default class Panel extends React.Component {
     );
   }
 }
+
+export default class MovieBrowser extends React.Component {
+  render() {
+    const currentPlayingTitle = 'Mad Max: Fury Road';
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        isPlaying: child.props.title === currentPlayingTitle
+      });
+    });
+ 
+    return (
+      <div className="movie-browser">
+        {childrenWithExtraProp}
+      </div>      
+    );
+  }
+}
