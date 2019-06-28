846f3fcd86924fa6b153535acb936ab2700b269d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-28 08:05:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// Code Goes Here
+export default class Panel extends React.Component {
+  render() {
+    return (
+      <div className="panel">
+        <div className="panel-header">{this.props.title}</div>
+        <div className="panel-body">{this.props.children}</div>
+      </div>
+    );
+  }
+}
